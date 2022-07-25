a51bf9d5f1b9e5c4e31b8f29a379127e612046cb
Abby Rosenthal <adlundeen@gmail.com>
2022-07-24 21:01:34 -0500

    functions

--- db/connection.js
+++ db/connection.js
@@ -6,12 +6,11 @@ require('dotenv').config();
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        port: 3306,
         //Your MySQL username,
-        user: 'root',
+        user: process.env.DB_USER,
         //Your MYSQL password
         password: process.env.DB_PASSWORD,
-        database: 'workplace'
+        database: process.env.DB_NAME
     },
     console.log('Connected to the workplace database.')
 );
--- server.js
+++ server.js
@@ -1,26 +1,6 @@
-
-const express = require('express');
 const db = require('./db/connection');
 const inquirer = require('inquirer');
-const { allowedNodeEnvironmentFlags } = require('process');
-
-const PORT = process.env.PORT || 3306;
-const app = express();
-
-//express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-//starts the express server on 3002
-// Start server after DB connection
-db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-});
+require('console.table');
 
 
 function askQuestions() {
@@ -59,14 +39,108 @@ function askQuestions() {
                 case "Add an Employee":
                     addEmployee();
                     break;
-                case "Update Employee Role":
-                    updateEmployee();
+                case "Update an Employee Role":
+                    updateEmployeeRole();
                     break;
-                case "I'm All Done! Goodbye":
+                default:
                     allDone();
-                    break;
             }
         })
 }
+
+function viewDepartments() {
+    //display table showing all departments
+};
+
+function viewRoles() {
+    //job title, role id, the department that role belongs to, and the salary for that role
+};
+
+function viewEmployees() {
+    //formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+    const sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, concat(manager.first_name,' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id"
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err)
+        }
+        console.table(res)
+        askQuestions();
+    })
+};
+
+function updateEmployeeRole() {
+    console.log('test')
+    //prompted to select an employee to update and their new role and this information is updated in the database
+    const sql = "SELECT * FROM employee"
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log(err)
+        }
+        const employees = res.map(({id, first_name, last_name})=>({
+           value: id,
+           name: `${first_name} ${last_name}`
+        }))
+        inquirer.prompt([{
+            type: "list",
+            name: "employee_id",
+            message: "Which employees' role do you want to update?",
+            choices: employees
+        }]   
+        )
+        .then(result => {
+            let employee_id = result.employee_id
+            const sql = "SELECT * FROM role"
+            db.query(sql, (err, res) => {
+                if (err) {
+                    console.log(err)
+                }
+                const roles = res.map(({ id, title }) =>({
+                    value: id,
+                    name: `${title}`
+                }))
+                updateRole(employee_id, roles)
+            })
+        })
+    })
+};
+
+function updateRole(employee_id, roles) {
+    inquirer.prompt ([{
+        type: "list",
+        name: "role",
+        message: "What is the updated role?",
+        choices: roles
+    }])
+    .then(result => {
+        const sql = "UPDATE employee SET role_id = ? WHERE id=?"
+        const params = [result.role, employee_id]
+        db.query(sql, params, (err, res) => {
+            if (err) {
+                console.log(err)
+            }
+            viewEmployees();
+        })
+    })
+}
+
+function addDepartment() {
+    //prompted to enter the name of the department and that department is added to the database
+};
+
+function addRole() {
+    //prompted to enter the name, salary, and department for the role and that role is added to the database
+};
+
+function addEmployee() {
+    //prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+};
+
+
+
+function allDone() {
+    console.log("Goodbye!")
+    process.exit();
+};
+
 
 askQuestions();
